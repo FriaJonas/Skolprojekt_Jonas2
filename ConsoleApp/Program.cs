7e4e9cc566683f0f0a18b460f8917f9dbfc5fd2c
Jonas Nilsson <jonas.nilsson@laroverken.se>
2022-08-30 11:07:27 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej - vad heter du ?");
+            string name = Console.ReadLine();
+            name = "Hej";
+            Console.WriteLine("Hej på dig " + name + " och grattis till ditt första program!");
+        }
+    }
+}
